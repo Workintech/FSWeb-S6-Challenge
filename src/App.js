6dc6d868013e88a1351d36b73f2fb00c4f966c4f
berkakaz <berk.akaz@gmail.com>
2023-03-06 00:48:08 +0300

    S6-Challange Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Karakter from "./components/Karakter";
+import Pagination from "@mui/material/Pagination";
+import { TextField } from "@mui/material";
 
 const App = () => {
+  const [charactersData, setCharactersData] = useState([]);
+  const [filmsData, setFilmsData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+
+  const charPerPage = 4;
+
+  useEffect(() => {
+    axios
+      .all([
+        axios.get(`https://swapi.dev/api/people/`),
+        axios.get(`https://swapi.dev/api/films/`),
+      ])
+      .then(
+        axios.spread((res1, res2) => {
+          // console.log(res1);
+          // console.log(res2);
+          return [res1.data, res2.data[0].results];
+        })
+      )
+      .catch((err) => console.log("Hata ile karsilasildi >", err))
+      .then(([data1, data2]) => {
+        console.log("data cekildi chars>", data1);
+        console.log("data cekildi films>", data2);
+        setCharactersData(data1);
+        setFilmsData(data2);
+      });
+  }, []);
+
+  const handleChange = (evt, val) => {
+    setPage(val);
+  };
+  const handleSearch = (evt) => {
+    setSearchText(evt.target.value);
+  };
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  // console.log(charactersData.slice(0, 2));
+  // console.log(charactersData.slice(2, 4));
+  // console.log(charactersData.slice(4, 6));
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <TextField
+        id="outlined-basic"
+        onChange={handleSearch}
+        label="Search"
+        variant="outlined"
+      />
+      {charactersData &&
+        charactersData
+          .filter((char) => char.name.includes(searchText))
+          .slice(charPerPage * (page - 1), charPerPage * page)
+          .map((char, index) => {
+            return (
+              <Karakter
+                key={"C" + index}
+                char={char}
+                films={filmsData.filter((film) =>
+                  char.films.includes(film.title)
+                )}
+              />
+            );
+          })}
+      <Pagination
+        count={Math.ceil(charactersData.length / charPerPage)}
+        page={page}
+        onChange={handleChange}
+        color="secondary"
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,96 @@
-// Karakter bileşeniniz buraya gelecek
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Box } from "@mui/material";
+
+export default function Karakter({ char, films }) {
+  const [expanded, setExpanded] = useState(false);
+  const [expandedSub, setExpandedSub] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+  const handleChangeSub = (panel) => (event, isExpanded) => {
+    setExpandedSub(isExpanded ? panel : false);
+  };
+
+  const CustomType = styled(Typography)`
+    text-align: left;
+    flex: 1;
+  `;
+
+  const CustomBox = styled(Box)`
+    margin-bottom: 0.5rem;
+    display: flex;
+    flex-wrap: wrap;
+  `;
+
+  return (
+    <Accordion
+      sx={{ background: "rgba(255, 255, 180, 0.5)" }}
+      expanded={expanded === "panel1"}
+      onChange={handleChange("panel1")}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1bh-content"
+        id="panel1bh-header"
+      >
+        <CustomType sx={{ width: "33%" }}>{char.name}</CustomType>
+        <CustomType sx={{ color: "text.secondary" }}>
+          {char.birth_year}
+        </CustomType>
+      </AccordionSummary>
+      <AccordionDetails>
+        <CustomBox>
+          <CustomType>Name : </CustomType>
+          {char.name}
+        </CustomBox>
+        <CustomBox>
+          <CustomType>Height : </CustomType>
+          {char.height}
+        </CustomBox>
+        <CustomBox>
+          <CustomType>Hair Color : </CustomType>
+          {char.hair_color}
+        </CustomBox>
+        <CustomBox>
+          <CustomType>Skin color : </CustomType>
+          {char.skin_color}
+        </CustomBox>
+        <CustomBox>
+          <CustomType>Eye color : </CustomType>
+          {char.eye_color}
+        </CustomBox>
+        {films.map((film, index) => {
+          return (
+            <Accordion
+              key={film.episode_id}
+              sx={{ background: "rgba(255, 255, 180, 0)" }}
+              expanded={expandedSub === "panel2" + index}
+              onChange={handleChangeSub("panel2" + index)}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={`panel2bh-content${index}`}
+                id={`panel2bh-header${index}`}
+              >
+                <CustomType>Appeared In : </CustomType>
+                {film.title}
+              </AccordionSummary>
+              <AccordionDetails>
+                <CustomBox>
+                  <CustomType>{film.opening_crawl}</CustomType>
+                </CustomBox>
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+      </AccordionDetails>
+    </Accordion>
+  );
+}
